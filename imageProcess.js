5a9cfc633406043febb98df86002cdda4823c059
Giuseppe Matteoni <giuseppe.matteoni@avanade.com>
2019-02-27 15:05:50 -0300

    upload e dimensionamento com canvas do modal fixo

--- imageProcess.js
+++ imageProcess.js
@@ -1,6 +1,9 @@
 //Variáveis de cstomização (Padrão Arial, 12)
 var fontSize = 12;
 var fontName = "Arial";
+var textVal;
+var dimLarg = localStorage.getItem("dimLarg");
+var dimAlt = localStorage.getItem("dimAlt");
 
 //Canvas pai
 let canvas = document.getElementById('canvasss');
@@ -10,29 +13,33 @@ let ctx = canvas.getContext("2d");
 let canvasModal = document.getElementById('canvasModal');
 let ctxModal = canvasModal.getContext("2d");
 
+//Proporções
+var propAlt = dimAlt/150;
+var propLarg = dimLarg/200;
+
 //Tamanho do canvas
-canvas.width = 600;
-canvas.height = 600;
+canvas.width = dimLarg;
+canvas.height = dimAlt;
 
 //Pega imagem do local storage
 let background = new Image();
 background.src = localStorage.getItem("formImage");
 
 background.onload = function () {
-    ctx.drawImage(background, 0, 0, 600, 600);
-} 
+    ctx.drawImage(background, 0, 0, dimLarg, dimAlt);
+}
 
 function buildImage(event) {
     let cordX = event.offsetX;
     let cordY = event.offsetY;
-    ctx.drawImage(background, 0, 0, 600, 600);
+    ctx.drawImage(background, 0, 0, dimLarg, dimAlt);
 
     $('#myModal').modal('show')
 
     buildImageModal(cordX, cordY);
 }
 
-function buildImageModal(cordX, cordY){
+function buildImageModal(cordX, cordY) {
     //Limpa o local storage
     localStorage.removeItem("modalCanvasX");
     localStorage.removeItem("modalCanvasY");
@@ -42,16 +49,16 @@ function buildImageModal(cordX, cordY){
     ctxModal.drawImage(background, 0, 0, 200, 150);
 
     //Renderiza retângulo
-    var cordXModal = cordX/3;
-    var cordYModal = cordY/4;
+    var cordXModal = cordX / this.propLarg;
+    var cordYModal = cordY / this.propAlt;
 
     //Insere no local storage 
     localStorage.setItem("modalCanvasX", cordXModal);
     localStorage.setItem("modalCanvasY", cordYModal);
-    ctxModal.strokeRect(cordXModal, cordYModal, 25, (this.fontSize/4));
+    ctxModal.strokeRect(cordXModal, cordYModal, 0.5, (this.fontSize / 4));
 }
 
-function modalClick(event){
+function modalClick(event) {
     let mouseX;
     let mouseY;
 
@@ -70,7 +77,7 @@ function modalClick(event){
     ctxModal.drawImage(background, 0, 0, 200, 150);
 
     //Redesenha
-    ctxModal.strokeRect(mouseX, mouseY, 25, (this.fontSize/4));
+    ctxModal.strokeRect(mouseX, mouseY, 0.5, (this.fontSize / 4));
 
     //Insere no local storage 
     localStorage.setItem("modalCanvasX", mouseX);
@@ -78,33 +85,48 @@ function modalClick(event){
 
 }
 
-function salvarModal(){
+function salvarModal() {
     //Desenha no canvas pai 
-        //Pega no local storage
-        var recoverX = localStorage.getItem("modalCanvasX");
-        var recoverY = localStorage.getItem("modalCanvasY");
+    //Pega no local storage
+    var recoverX = localStorage.getItem("modalCanvasX");
+    var recoverY = localStorage.getItem("modalCanvasY");
 
-        //Desenha
-        ctx.strokeRect(recoverX * 3, recoverY * 4, 100, (this.fontSize));
+    //Altera textVal
+    this.textVal = document.getElementById('textVal').value;
+
+    //Desenha
+    ctx.font = `${this.fontSize}px ${this.fontName}`;
+    ctx.fillText(this.textVal, recoverX * this.propLarg, parseInt(recoverY) * this.propAlt + parseInt(this.fontSize));
 
     $('#myModal').modal('hide');
 }
 
-function cancelarModal(){
+function cancelarModal() {
     //Limpa o local storage
     localStorage.removeItem("modalCanvasX");
     localStorage.removeItem("modalCanvasY");
     $('#myModal').modal('hide');
 }
 
-function setSize(event){
+function setSize(event) {
+    //Altera fontSize
     this.fontSize = event.target.value;
-    console.log(this.fontSize);
+
+    //Limpa o canvas
+    ctxModal.clearRect(0, 0, canvas.width, canvas.height);
+    //Insere o background
+    ctxModal.drawImage(background, 0, 0, 200, 150);
+
+    //Pega local storage
+    let recoverX = localStorage.getItem("modalCanvasX");
+    let recoverY = localStorage.getItem("modalCanvasY");
+
+    //Redesenha com tamanho alterado
+    ctxModal.strokeRect(recoverX, recoverY, 0.5, (this.fontSize / 4));
 }
 
-function setFont(event){
+function setFont(event) {
     this.fontName = event.target.value;
-    console.log(this.fontName);
 }
 
 
